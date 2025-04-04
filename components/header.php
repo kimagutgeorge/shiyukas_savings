8d7af30eb7e18db13a6b1031c6661bd61cd9e8fe
George Kimagut <geojimagut@gmail.com>
2025-04-04 15:12:29 +0300

    <navigation UI/fontawesome icons>

--- components/header.php
+++ components/header.php
@@ -4,5 +4,6 @@
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Family Chama</title>
-    <link rel='stylesheet' href='styling/style.css'>
+    <link rel='stylesheet' href="styling/style.css?v=<?php echo time();?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
--- components/navbar.php
+++ components/navbar.php
@@ -1,12 +1,17 @@
 <div class="nav-bar">
-        <li><a href='dashboard.php'>Dashboard</a></li>
-        <li><a href='contributions.php'>Add Contribution</a></li>
-        <li><a href='loans.php'>Add Loan</a></li>
-        <li><a href='loan_repayments.php'>Pay Loan</a></li>
-        <li><strong>Reports:</strong></li>
-        <li><a href='loan_history.php'>Loan History</a></li>
-        <li><a href='loan_report.php'>Loan Repayments</a></li>
-        <li><a href='loan_summaries.php'>Loan Summaries</a></li>
-        <li><a href='contributions_history.php'>Contributions History</a></li>
-        <li><a href='login.php'>Logout</a></li>
+        <div class="nav-bar-inner">
+        <li><a href='dashboard.php'><i class="fa-solid fa-house"></i> Dashboard</a></li>
+        <li><a href='contributions.php'><i class="fa-solid fa-plus"></i> Add Contribution</a></li>
+        <li><a href='loans.php'><i class="fa-solid fa-plus"></i>  Add Loan</a></li>
+        <li><a href='loan_repayments.php'><i class="fa-solid fa-money-bill"></i> Pay Loan</a></li>
+        <li class="w-border"></li>
+        <li><a href='loan_history.php'><i class="fa-solid fa-list"></i> Loan History</a></li>
+        <li><a href='loan_report.php'><i class="fa-solid fa-list"></i> Loan Repayments</a></li>
+        <li><a href='loan_summaries.php'><i class="fa-solid fa-list"></i> Loan Summaries</a></li>
+        <li><a href='contributions_history.php'><i class="fa-solid fa-list"></i> Contributions History</a></li>
+        
+</div>
+<div class="nav-bar-inner-settings">
+<li><a href='login.php'><i class="fa-solid fa-power-off"></i> Logout</a></li>
+</div>
 </div>
